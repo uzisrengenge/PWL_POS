47ec340854138950f07233e40b98246caf2bd8ea
uzidreamcode <77190656+uzidreamcode@users.noreply.github.com>
2024-05-15 22:56:49 +0700

    feat: Add logout API endpoint

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
 
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LogoutController;
+
 
 
 /*
@@ -27,3 +29,5 @@ Route::post('/login', LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/logout', LogoutController::class)->name('logout');
+
